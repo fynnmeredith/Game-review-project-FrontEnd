9a0fed121c4ee79441a6f00d28ccbade7711bd1f
fynnmeredith <fynnmeredith2@gmail.com>
2022-02-13 12:18:04 +0000

    dislike button

--- src/components/Patch_Votes/PatchVotes.jsx
+++ src/components/Patch_Votes/PatchVotes.jsx
@@ -4,6 +4,7 @@ import "./PatchVotes.css";
 
 const PatchVotes = ({ review }) => {
   const [vote, setVote] = useState(0);
+  const [minusVote, setMinusVote] = useState(0)
 
   const giveVote = () => {
     setVote((currValue) => currValue + 1);
@@ -12,11 +13,23 @@ const PatchVotes = ({ review }) => {
     document.getElementById("oneVote").disabled = true;
   };
 
+  const takeVote = () => {
+    setMinusVote((currValue) => currValue - 1);
+    patchVotes(review.review_id, vote);
+    review.votes -= 1;
+    document.getElementById("minusVote").disabled = true;
+  };
+
   return (
     <>
+    <div className="likeIt">
       <button className="upvote" onClick={() => giveVote()} id="oneVote">
-        {review.votes} 👍
+       👍
       </button>
+      <p>{review.votes}</p>
+      <button className="downvote" onClick={() => takeVote()} id="minusVote">👎</button>
+
+    </div>
     </>
   );
 };
